af329be0eab75ba398dd68801635fdef4bb4bab3
metelykx <ivaschenkoodenys@gmail.com>
2025-03-04 19:03:43 +0300

    comment

--- narutoo/Model/Shinobi.swift
+++ narutoo/Model/Shinobi.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 struct Shinobi: Identifiable {
-    let id: UUID
-    let name: String
-    let clan: String
-    let village: String
-    let power: Int
-    let intelligence: Int
-    let Ninjutsu: Int
-    let element: String
-    let Gendzusu: String
-    let Taidzusu: String
+    let id: UUID //id - героя
+    let name: String // имя героя
+    let clan: String // клан героя
+    let village: String // деревня героя
+    let power: Int // сила героя
+    let intelligence: Int // интеллект героя
+    let Ninjutsu: Int // нинджутсу героя
+    let element: String // стихия героя
+    let Gendzusu: String // гендюцу героя
+    let Taidzusu: String // тайдзюцу героя
     
 }
