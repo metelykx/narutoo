adfa07f8acc5f8ff2933ed504e389469f7da66fb
metelykx <ivaschenkoodenys@gmail.com>
2025-03-04 17:37:26 +0300

    Initial Commit

---
+++ narutoo/narutooApp.swift
@@ -0,0 +1,17 @@
+//
+//  narutooApp.swift
+//  narutoo
+//
+//  Created by Denis Ivaschenko on 04.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct narutooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ narutooTests/narutooTests.swift
@@ -0,0 +1,17 @@
+//
+//  narutooTests.swift
+//  narutooTests
+//
+//  Created by Denis Ivaschenko on 04.03.2025.
+//
+
+import Testing
+@testable import narutoo
+
+struct narutooTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
