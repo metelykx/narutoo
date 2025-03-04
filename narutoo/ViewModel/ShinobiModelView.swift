9cc731b9b398b3d44fcffcd9a554c319bbe8c23c
metelykx <ivaschenkoodenys@gmail.com>
2025-03-04 19:23:12 +0300

    singelton

---
+++ narutoo/ViewModel/ShinobiModelView.swift
@@ -0,0 +1,13 @@
+//
+//  ShinobiModelView.swift
+//  narutoo
+//
+//  Created by Denis Ivaschenko on 04.03.2025.
+//
+
+import Foundation
+
+class ShinobiModelView: ObservableObject {
+    //сингелтон, для создания единственного экземпляра класса
+    static let shared = ShinobiModelView()
+}
