fffb248573e94b6c48b0e16ba7e85f2797ae136a
metelykx <ivaschenkoodenys@gmail.com>
2025-03-04 22:20:27 +0300

    sasuke

--- narutoo/ViewModel/ShinobiModelView.swift
+++ narutoo/ViewModel/ShinobiModelView.swift
@@ -12,7 +12,9 @@ class ShinobiModelView: ObservableObject {
     static let shared = ShinobiModelView()
     
     var shinobi = [
-        Shinobi(id: 1, name: "Naruto Uzumaki", clan: "Uzumaki", village: "Konohagakure", power: 95, specialPower: "Nine-tailed Fox Demon (Kyubi) — Inexhaustible Chakra, Sage Mode", intelligence: 75, Ninjutsu: 90, element: "Wind", Gendzusu: 40, Taidzusu: 85)
+        Shinobi(id: 1, name: "Naruto Uzumaki", clan: "Uzumaki", village: "Konohagakure", power: 95, specialPower: "Nine-tailed Fox Demon (Kyubi) — Inexhaustible Chakra, Sage Mode", intelligence: 75, Ninjutsu: 90, element: "Wind", Gendzusu: 40, Taidzusu: 85),
+        
+        Shinobi(id: 2, name: "Sasuke Uchiha", clan: "Uchiha", village: "Konohagakure", power: 95, specialPower: "Eternal Mangekyo Sharingan and Rinegan — Space Control, Amaterasu (Unquenchable flame)", intelligence: 88, Ninjutsu: 95, element: "Fire, Lightning", Gendzusu: 95, Taidzusu: 90)
         
         
     ]
