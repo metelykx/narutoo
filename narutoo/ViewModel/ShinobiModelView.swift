825a3a6ddeefafc5ac90000abf1102a5ff3c0f04
metelykx <ivaschenkoodenys@gmail.com>
2025-03-05 20:03:30 +0300

    sasuke img

--- narutoo/ViewModel/ShinobiModelView.swift
+++ narutoo/ViewModel/ShinobiModelView.swift
@@ -14,7 +14,7 @@ class ShinobiModelView: ObservableObject {
     var shinobi = [
         Shinobi(id: 1,imageUrl: "naruto", name: "Naruto Uzumaki", clan: "Uzumaki", village: "Konohagakure", power: 95, specialPower: "Nine-tailed Fox Demon (Kyubi) — Inexhaustible Chakra, Sage Mode", intelligence: 75, Ninjutsu: 90, element: "Wind", Gendzusu: 40, Taidzusu: 85),
         
-        Shinobi(id: 2, imageUrl: "", name: "Sasuke Uchiha", clan: "Uchiha", village: "Konohagakure", power: 95, specialPower: "Eternal Mangekyo Sharingan and Rinegan — Space Control, Amaterasu (Unquenchable flame)", intelligence: 88, Ninjutsu: 95, element: "Fire, Lightning", Gendzusu: 95, Taidzusu: 90),
+        Shinobi(id: 2, imageUrl: "sasuke", name: "Sasuke Uchiha", clan: "Uchiha", village: "Konohagakure", power: 95, specialPower: "Eternal Mangekyo Sharingan and Rinegan — Space Control, Amaterasu (Unquenchable flame)", intelligence: 88, Ninjutsu: 95, element: "Fire, Lightning", Gendzusu: 95, Taidzusu: 90),
         
         Shinobi(id: 3, imageUrl: "", name: "Kakashi Hatake", clan: "Hatake", village: "Konohagakure", power: 90, specialPower: "Sharingan — copying techniques, Kaminu (lightning piercing the sky)", intelligence: 97, Ninjutsu: 92, element: "Lightning", Gendzusu: 85, Taidzusu: 88),
         
