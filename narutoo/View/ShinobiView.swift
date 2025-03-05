e5525aaec52b5e33dc9f168666964afd2316b31e
metelykx <ivaschenkoodenys@gmail.com>
2025-03-05 15:13:48 +0300

    new view

---
+++ narutoo/View/ShinobiView.swift
@@ -0,0 +1,18 @@
+//
+//  ShinoviView.swift
+//  narutoo
+//
+//  Created by Denis Ivaschenko on 05.03.2025.
+//
+
+import SwiftUI
+
+struct ShinobiView: View {
+    var body: some View {
+        Text("salam")
+    }
+}
+
+#Preview {
+    ShinobiView()
+}
