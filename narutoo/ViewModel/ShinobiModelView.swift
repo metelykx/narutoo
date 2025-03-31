030dc74473ead24b3ad0d6c155dc038bc34f8aad
metelykx <ivaschenkoodenys@gmail.com>
2025-03-31 22:33:41 +0300

    global queue

--- narutoo/ViewModel/ShinobiModelView.swift
+++ narutoo/ViewModel/ShinobiModelView.swift
@@ -29,8 +29,15 @@ class ShinobiModelView: ObservableObject {
     }
     
     
-    
-        var shinobi = [
+    func loadShinobiData() {
+        isLoading = true
+        isError = nil
+        
+        
+        DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+        }
+    }
+    var shinobi = [
         Shinobi(id: 1,imageUrl: "naruto", name: "Naruto", clan: "Uzumaki", village: "Konohagakure", power: 95, specialPower: "Nine-tailed Fox Demon (Kyubi) — Inexhaustible Chakra, Sage Mode", intelligence: 75, Ninjutsu: 90, element: "Wind", Gendzusu: 40, Taidzusu: 85),
         
         Shinobi(id: 2, imageUrl: "sasuke", name: "Sasuke", clan: "Uchiha", village: "Konohagakure", power: 95, specialPower: "Eternal Mangekyo Sharingan and Rinegan — Space Control, Amaterasu (Unquenchable flame)", intelligence: 88, Ninjutsu: 95, element: "Fire, Lightning", Gendzusu: 95, Taidzusu: 90),
