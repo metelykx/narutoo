148ab63eba545cb11520e35f30f7567fe43f8ffb
metelykx <ivaschenkoodenys@gmail.com>
2025-04-04 22:05:29 +0300

    new vie gods and villain

---
+++ narutoo/View/ShinobiGodsListView.swift
@@ -0,0 +1,14 @@
+//
+//  ShinobiGodsListView.swift
+//  narutoo
+//
+//  Created by Denis Ivaschenko on 04.04.2025.
+//
+
+import SwiftUI
+
+struct ShinobiGodsListView: View {
+    var body: some View {
+        Text("gg")
+    }
+}
---
+++ narutoo/View/ShinobiVillainListView.swift
@@ -0,0 +1,14 @@
+//
+//  ShinobiVillainListView.swift
+//  narutoo
+//
+//  Created by Denis Ivaschenko on 04.04.2025.
+//
+
+import SwiftUI
+
+struct ShinobiVillainListView: View {
+    var body: some View {
+        Text("Shinobi Villain List View")
+    }
+}
