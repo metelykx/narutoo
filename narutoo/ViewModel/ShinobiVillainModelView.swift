65e66759217a4d8a029865de3b5970fa8fe97c02
metelykx <ivaschenkoodenys@gmail.com>
2025-04-07 22:35:51 +0300

    reprog

--- narutoo/ViewModel/ShinobiVillainModelView.swift
+++ narutoo/ViewModel/ShinobiVillainModelView.swift
@@ -1,41 +1,71 @@
-//
-//  ShinobiVillains.swift
-//  narutoo
-//
-//  Created by Denis Ivaschenko on 07.04.2025.
-//
-
 import Foundation
-
 class ShinobiVillainModelView: ObservableObject {
-    
-    //исльзуем сингелтон паттерн проектирования
     static let shared = ShinobiVillainModelView()
     
     @Published var villains: [Shinobi] = []
-    
     @Published var isLoading: Bool = false
-    
     @Published var isError: Error? = nil
-    
     
     private init() {
         loadVillain()
     }
     
-    
     func loadVillain() {
         isLoading = true
         isError = nil
         
-        
-        DispatchQueue.global(qos: .userInitiated).async {
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             let villains = [
-                Shinobi(id: 16, imageUrl: "kakuzu", name: "Kakuzu", clan: "Absent", village: "Still", power: 93, specialPower: "\("Five hearts") (control of 5 elements, immortality).", intelligence: 95, Ninjutsu: 89, element: "All five elements.", Gendzusu: 0, Taidzusu: 99),
-                Shinobi(id: 17, imageUrl: "kisame", name: "Kisame", clan: "Hoshigaki", village: "Kiri", power: 94, specialPower: "Merging with Samehada (chakra absorption)", intelligence: 85, Ninjutsu: 91, element: "Water", Gendzusu: 0, Taidzusu: 100),
-                Shinobi(id: 18, imageUrl: "konan", name: "Konan", clan: "Absent", village: "Amegakure", power: 90, specialPower: "Paper techniques", intelligence: 96, Ninjutsu: 95, element: "Water and Wind", Gendzusu: 0, Taidzusu: 40)
+                Shinobi(
+                    id: 16,
+                    imageUrl: "kakuzu",
+                    name: "Kakuzu",
+                    clan: "Absent",
+                    village: "Takigakure",  
+                    power: 93,
+                    specialPower: "Five Hearts (control of 5 elements, immortality)",
+                    intelligence: 95,
+                    Ninjutsu: 89,
+                    element: "All five elements",
+                    Gendzusu: 0,
+                    Taidzusu: 99
+                ),
+                Shinobi(
+                    id: 17,
+                    imageUrl: "kisame",
+                    name: "Kisame",
+                    clan: "Hoshigaki",
+                    village: "Kirigakure",
+                    power: 94,
+                    specialPower: "Samehada Fusion (chakra absorption)",
+                    intelligence: 85,
+                    Ninjutsu: 91,
+                    element: "Water",
+                    Gendzusu: 0,
+                    Taidzusu: 100
+                ),
+                Shinobi(
+                    id: 18,
+                    imageUrl: "konan",
+                    name: "Konan",
+                    clan: "None",
+                    village: "Amegakure",
+                    power: 90,
+                    specialPower: "Paper Ninjutsu",
+                    intelligence: 96,
+                    Ninjutsu: 95,
+                    element: "Water, Wind",
+                    Gendzusu: 0,
+                    Taidzusu: 40
+                )
             ]
+            
+            Thread.sleep(forTimeInterval: 2)
+            
+            DispatchQueue.main.async {
+                self?.villains = villains
+                self?.isLoading = false
+            }
         }
-       
     }
 }
