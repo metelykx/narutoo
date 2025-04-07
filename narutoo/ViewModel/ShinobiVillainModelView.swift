575f9fa9179ccae99c55b4818f3ee5cf08bc93ae
metelykx <ivaschenkoodenys@gmail.com>
2025-04-07 21:56:37 +0300

    multith

--- narutoo/ViewModel/ShinobiVillainModelView.swift
+++ narutoo/ViewModel/ShinobiVillainModelView.swift
@@ -12,7 +12,7 @@ class ShinobiVillainModelView: ObservableObject {
     //исльзуем сингелтон паттерн проектирования
     static let shared = ShinobiVillainModelView()
     
-    @Published var shinobi: [Shinobi] = []
+    @Published var villains: [Shinobi] = []
     
     @Published var isLoading: Bool = false
     
@@ -25,6 +25,15 @@ class ShinobiVillainModelView: ObservableObject {
     
     
     func loadVillain() {
+        isLoading = true
+        isError = nil
         
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let villains = [
+                Shinobi(id: 16, imageUrl: <#T##String#>, name: <#T##String#>, clan: <#T##String#>, village: <#T##String#>, power: <#T##Int#>, specialPower: <#T##String#>, intelligence: <#T##Int#>, Ninjutsu: <#T##Int#>, element: <#T##String#>, Gendzusu: <#T##Int#>, Taidzusu: <#T##Int#>)
+            ]
+        }
+       
     }
 }
