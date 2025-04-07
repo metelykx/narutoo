8149d466572b29440172fa8d8200247c6d6a0644
metelykx <ivaschenkoodenys@gmail.com>
2025-04-07 22:57:56 +0300

    new images

--- narutoo/ViewModel/ShinobiGodsModelView.swift
+++ narutoo/ViewModel/ShinobiGodsModelView.swift
@@ -13,10 +13,32 @@ class ShinobiGodsModelView: ObservableObject {
     // singelton
     static let shared = ShinobiGodsModelView()
     
+    
     //MARK: variables for information,changes
+    
     @Published var gods: [Shinobi] = []
     
     @Published var isLoading: Bool = false
     
     @Published var isError: Error? = nil
+    
+    //при инициализации идет загрузка
+    private init() {
+        loadGods()
+    }
+    
+    func loadGods() {
+        
+        isLoading = true
+        isError = nil
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let shinobiGods = {
+                Shinobi(id: 19, imageUrl: <#T##String#>, name: <#T##String#>, clan: <#T##String#>, village: <#T##String#>, power: <#T##Int#>, specialPower: <#T##String#>, intelligence: <#T##Int#>, Ninjutsu: <#T##Int#>, element: <#T##String#>, Gendzusu: <#T##Int#>, Taidzusu: <#T##Int#>)
+                Shinobi(id: 20, imageUrl: <#T##String#>, name: <#T##String#>, clan: <#T##String#>, village: <#T##String#>, power: <#T##Int#>, specialPower: <#T##String#>, intelligence: <#T##Int#>, Ninjutsu: <#T##Int#>, element: <#T##String#>, Gendzusu: <#T##Int#>, Taidzusu: <#T##Int#>)
+                Shinobi(id: 21, imageUrl: <#T##String#>, name: <#T##String#>, clan: <#T##String#>, village: <#T##String#>, power: <#T##Int#>, specialPower: <#T##String#>, intelligence: <#T##Int#>, Ninjutsu: <#T##Int#>, element: <#T##String#>, Gendzusu: <#T##Int#>, Taidzusu: <#T##Int#>)
+            }
+        }
+        
+    }
 }
