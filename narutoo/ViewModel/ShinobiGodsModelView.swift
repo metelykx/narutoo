cd3eafd98ecc7df5053e0e24d13e6938c0499ddd
metelykx <ivaschenkoodenys@gmail.com>
2025-04-07 23:13:36 +0300

    new modelView

--- narutoo/ViewModel/ShinobiGodsModelView.swift
+++ narutoo/ViewModel/ShinobiGodsModelView.swift
@@ -32,11 +32,65 @@ class ShinobiGodsModelView: ObservableObject {
         isLoading = true
         isError = nil
         
-        DispatchQueue.global(qos: .userInitiated).async {
-            let shinobiGods = {
-                Shinobi(id: 19, imageUrl: <#T##String#>, name: <#T##String#>, clan: <#T##String#>, village: <#T##String#>, power: <#T##Int#>, specialPower: <#T##String#>, intelligence: <#T##Int#>, Ninjutsu: <#T##Int#>, element: <#T##String#>, Gendzusu: <#T##Int#>, Taidzusu: <#T##Int#>)
-                Shinobi(id: 20, imageUrl: <#T##String#>, name: <#T##String#>, clan: <#T##String#>, village: <#T##String#>, power: <#T##Int#>, specialPower: <#T##String#>, intelligence: <#T##Int#>, Ninjutsu: <#T##Int#>, element: <#T##String#>, Gendzusu: <#T##Int#>, Taidzusu: <#T##Int#>)
-                Shinobi(id: 21, imageUrl: <#T##String#>, name: <#T##String#>, clan: <#T##String#>, village: <#T##String#>, power: <#T##Int#>, specialPower: <#T##String#>, intelligence: <#T##Int#>, Ninjutsu: <#T##Int#>, element: <#T##String#>, Gendzusu: <#T##Int#>, Taidzusu: <#T##Int#>)
+        func loadGods() {
+            isLoading = true
+            isError = nil
+            
+            DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+                // Создаем массив Shinobi
+                let shinobiGods = [
+                    Shinobi(
+                        id: 19,
+                        imageUrl: "toneri",
+                        name: "Toneri",
+                        clan: "Ootsutsuki",
+                        village: "Absent",
+                        power: 70,
+                        specialPower: "Tenseigan",
+                        intelligence: 60,
+                        Ninjutsu: 90,
+                        element: "Wind, Light",
+                        Gendzusu: 10,
+                        Taidzusu: 30
+                    ),
+                    Shinobi(
+                        id: 20,
+                        imageUrl: "indra",
+                        name: "Indra",
+                        clan: "Ootsutsuki",
+                        village: "Absent",
+                        power: 90,
+                        specialPower: "Heir to the Hagoromo Chakra",
+                        intelligence: 99,
+                        Ninjutsu: 90,
+                        element: "Fire, Lightning",  // Исправлено на Lightning
+                        Gendzusu: 90,
+                        Taidzusu: 90
+                    ),
+                    Shinobi(
+                        id: 21,
+                        imageUrl: "hagoromo",
+                        name: "Hagoromo",
+                        clan: "Ootsutsuki",
+                        village: "Absent",
+                        power: 101,
+                        specialPower: "Rinnegan, Jubi Jinchuuriki, Creation of the Moon",
+                        intelligence: 101,
+                        Ninjutsu: 101,
+                        element: "All elements",
+                        Gendzusu: 101,
+                        Taidzusu: 80
+                    )
+                ]
+                
+                // Имитация задержки загрузки
+                Thread.sleep(forTimeInterval: 2)
+                
+                // Обновление UI в главном потоке
+                DispatchQueue.main.async {
+                    self?.gods = shinobiGods
+                    self?.isLoading = false
+                }
             }
         }
         
