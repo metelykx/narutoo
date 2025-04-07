b47d9bb68321e895f26a587dce44005f38c5081c
metelykx <ivaschenkoodenys@gmail.com>
2025-04-07 21:51:50 +0300

    add singelton, init

--- narutoo/ViewModel/ShinobiVillainModelView.swift
+++ narutoo/ViewModel/ShinobiVillainModelView.swift
@@ -11,5 +11,20 @@ class ShinobiVillainModelView: ObservableObject {
     
     //исльзуем сингелтон паттерн проектирования
     static let shared = ShinobiVillainModelView()
-    @Published var
+    
+    @Published var shinobi: [Shinobi] = []
+    
+    @Published var isLoading: Bool = false
+    
+    @Published var isError: Error? = nil
+    
+    
+    private init() {
+        loadVillain()
+    }
+    
+    
+    func loadVillain() {
+        
+    }
 }
